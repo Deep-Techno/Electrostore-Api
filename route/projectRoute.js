cbb6d4df506d1e0c9a0a8847be1f510f20cd3826
baqar naqvi <96628989+naqvi1122@users.noreply.github.com>
2023-01-25 12:44:48 +0500

    make api to show all  product  on page and  a api to delete product from page
    make two api one to show all product detail and other is to delete product  from page and database also make jwt check middleware to check jwt

--- route/projectRoute.js
+++ route/projectRoute.js
@@ -1,16 +1,19 @@
-const express=require('express')
-const {signup,login}=require('../controller/loginAndSignupController')
-const{storeProduct}=require('../controller/productController')
+const express = require('express')
+const { signup, login } = require('../controller/loginAndSignupController')
+const { storeProduct, all_product_details, deleteProduct } = require('../controller/productController')
 const multer = require('multer');
 const fs = require('fs');
-const storage=require('../configuration/multer.js')
-const router=express.Router();
+const storage = require('../configuration/multer.js')
+const router = express.Router();
+const jwtMiddleware = require('../middleware/jwtMiddleware');
 
-const upload = multer(storage );
+const upload = multer(storage);
 
-router.post('/signup',signup);
-router.post('/login',login);
-router.post('/api/product/store-data',upload.single('image'),storeProduct);
+router.post('/signup', signup);
+router.post('/login', login);
+router.post('/api/product/store-data', upload.single('image'), storeProduct);
+router.get('/api/product/productDetails', jwtMiddleware, all_product_details);
+router.delete('/api/product/deleteproduct/:id', jwtMiddleware, deleteProduct);
 
 
-module.exports={router};
+module.exports = { router };
