aa4ff9aa356ce5921825667e2ea7655b28038923
baqar naqvi <96628989+naqvi1122@users.noreply.github.com>
2023-02-16 13:28:39 +0500

    jwt middleware added

--- routes/projectRoute.js
+++ routes/projectRoute.js
@@ -12,10 +12,10 @@ const upload = multer(storage);
 router.post('/signup', signup);
 router.post('/login', login);
 router.post('/api/product/store-data', upload.single('image'), storeProduct);
-router.get('/api/product/productDetails',all_product_details);
+router.get('/api/product/productDetails',jwtMiddleware,all_product_details);
 router.delete('/api/product/deleteproduct/:id', deleteProduct);
-router.get('/api/singleproduct/:id', Singleproduct);
-router.patch('/api/productupdata/:id', Updataproduct);
+router.get('/api/singleproduct/:id',jwtMiddleware, Singleproduct);
+router.patch('/api/productupdata/:id',jwtMiddleware, Updataproduct);
 
 
 module.exports = { router };
